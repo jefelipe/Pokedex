554a85d42586b81d37bff730631c7c4342ade756
JeanFelipe <ermelindo35@hotmail.com>
2023-08-29 08:45:01 -0300

    Convertendo o modelo do PokeApi para nosso modelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,18 @@
 
 
-
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-          <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+          <span class="number">${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
 
           <div class="detail">
             <ol class="types">
-              <li class="type">glass</li>
-              <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
 
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              src="${pokemon.photo}"
               alt="${pokemon.name}"
             />
           </div>
@@ -33,6 +31,3 @@ pokeApi.getPokemons().then((pokemons = []) => {
 })
 
 
-
-
-  .catch((error) => console.error(error)); // Capturando e exibindo erros, caso ocorram.
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,11 +2,36 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+
+    const types = pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     // Construindo a URL da API usando template literals para inserir os valores de offset e limit.
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json()) // Convertendo a resposta para formato JSON.
         .then((jsonBody) => jsonBody.results) // Extraindo a lista de resultados do JSON.
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
+
